65a91958ed916546649ea97543ab919f88472bcd
rbparagi <rbparagi@gmail.com>
2025-01-04 22:41:08 +0530

    comming backend tf file

--- Terraform-scripts/network-setup/backend.tf
+++ Terraform-scripts/network-setup/backend.tf
@@ -4,7 +4,7 @@ terraform {
         key = "create-ec2-instance/terraform.tfstate"
         region = "ap-south-1"
         dynamodb_table = "terraform-locks"
-        encrypt = true
+        encrypt = false
     }
   
 }
